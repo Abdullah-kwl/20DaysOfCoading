b50d232bfe74c167b38c7e0c05b8cbdb6eae7fd9
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-03-12 21:45:41 +0500

    Day 03: Django ORM

--- tutorial-05/day03/app1/admin.py
+++ tutorial-05/day03/app1/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from app1.models import Profile
 # Register your models here.
+
+# admin.site.register(Profile)
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'city')
