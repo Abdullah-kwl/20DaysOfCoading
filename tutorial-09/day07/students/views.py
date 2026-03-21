574ce7cdd5fe0f7068fb09a8d38b078bd2e69c2b
Abdullah-kwl <mrabdullahkwl@gmail.com>
2026-03-21 10:32:03 +0500

    Day 07: learn Dynamic url

--- tutorial-09/day07/students/views.py
+++ tutorial-09/day07/students/views.py
@@ -3,6 +3,9 @@ from students.models import Profile
 
 # Create your views here.
 def home(request):
+    all_profiles = Profile.objects.all()
+    if all_profiles:
+        return render(request, 'students/home.html', context={'profiles': all_profiles})
     return render(request, 'students/home.html')
 
 def profile(request,roll_number):
